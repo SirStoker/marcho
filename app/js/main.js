e6391385c1f6aeca2c2cce1077e0eb72958ce88c
Владимир Дро <vladimirdro@MacBook-Air-Vladimir.local>
2023-11-29 10:47:27 +0100

    added on Shop Page -> Filters box

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function () {
 
+    $(".filter-prise__input").ionRangeSlider({
+        type: "double",
+        onStart: function (data) {
+            $('.filter-prise-from').text(data.from);
+            $('.filter-prise-to').text(data.to);
+        },
+        onChange: function (data) {
+            $('.filter-prise-from').text(data.from);
+            $('.filter-prise-to').text(data.to);
+        },
+    });
+
+
     $('.top-slider__inner').slick({
         arrows: false,
         dots: true,
@@ -15,48 +28,49 @@ $(function () {
         readOnly: true
     });
 
-    // function getTimeRemaining(endtime) {
-    //     const total = Date.parse(endtime) - Date.parse(new Date());
-    //     const seconds = Math.floor((total / 1000) % 60);
-    //     const minutes = Math.floor((total / 1000 / 60) % 60);
-    //     const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-    //     const days = Math.floor(total / (1000 * 60 * 60 * 24));
+    function getTimeRemaining(endtime) {
+        const total = Date.parse(endtime) - Date.parse(new Date());
+        const seconds = Math.floor((total / 1000) % 60);
+        const minutes = Math.floor((total / 1000 / 60) % 60);
+        const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+        const days = Math.floor(total / (1000 * 60 * 60 * 24));
 
-    //     return {
-    //         total,
-    //         days,
-    //         hours,
-    //         minutes,
-    //         seconds
-    //     };
-    // }
+        return {
+            total,
+            days,
+            hours,
+            minutes,
+            seconds
+        };
+    }
 
-    // function initializeClock(id, endtime) {
-    //     const clock = document.querySelector('.promo__clock');
-    //     const daysSpan = clock.querySelector('.promo__days');
-    //     const hoursSpan = clock.querySelector('.promo__hours');
-    //     const minutesSpan = clock.querySelector('.promo__minutes');
-    //     const secondsSpan = clock.querySelector('.promo__seconds');
+    function initializeClock(id, endtime) {
+        const clock = document.querySelector('.promo__clock');
+        const daysSpan = clock.querySelector('.promo__days');
+        const hoursSpan = clock.querySelector('.promo__hours');
+        const minutesSpan = clock.querySelector('.promo__minutes');
+        const secondsSpan = clock.querySelector('.promo__seconds');
 
-    //     function updateClock() {
-    //         const t = getTimeRemaining(endtime);
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
 
-    //         daysSpan.innerHTML = t.days;
-    //         hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    //         minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    //         secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+            daysSpan.innerHTML = t.days;
+            hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+            minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+            secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
-    //         if (t.total <= 0) {
-    //             clearInterval(timeinterval);
-    //         }
-    //     }
+            if (t.total <= 0) {
+                clearInterval(timeinterval);
+            }
+        }
 
-    //     updateClock();
-    //     const timeinterval = setInterval(updateClock, 1000);
-    // }
+        updateClock();
+        const timeinterval = setInterval(updateClock, 1000);
+    }
 
-    // const deadline = $('.promo__clock').attr('data-time');
-    // initializeClock('promo__clock', deadline);
+    const deadline = $('.promo__clock').attr('data-time');
+    initializeClock('promo__clock', deadline);
+
 
 
 });
